08a5567c3599e79deb9b3c8f7a983364e1732a6a
Ademar Neto <167026763+devAdemarNeto@users.noreply.github.com>
2025-08-16 23:50:41 -0300

    Adiciona classe Calculadora

---
+++ mes-1-orientacao-objetos/calculadora-basica/Calculadora.java
@@ -0,0 +1,21 @@
+package Calc;
+
+public class Calculadora {
+
+    public int soma(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int subtracao (int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiplicacao (int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divisao (int num1, int num2) {
+        return num1 / num2;
+    }
+
+}
